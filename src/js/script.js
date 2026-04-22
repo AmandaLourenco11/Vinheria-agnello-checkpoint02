a5ae572e89cbc14358c6194d593168bc1721db93
Giovanna <giscalzone@gmail.com>
2026-04-22 15:56:10 -0300

    feat: Estrutura de validar entrada e estrutura de while criada

--- src/js/script.js
+++ src/js/script.js
@@ -32,3 +32,34 @@ console.log(safra);
 let estoque = prompt('Insira a quantidade em estoque: ');
 alert('Estoque do vinho registrado! Veja os detalhes no console');
 console.log(estoque);
+
+let continuar = prompt("Deseja cadastrar outro vinho (s/n)? ");
+let totalCadastros = 0;
+let estoqueBaixo = 0;
+
+function validarEntrada(valor, mensagem) {
+    while (valor === "" || valor === null || valor === undefined) {
+        valor = prompt(mensagem);
+    }
+    return valor;
+}
+
+while (continuar === s) {
+
+    let nomeVinho = validarEntrada(prompt("Digite o nome do vinho:"), "Digite o nome do vinho:");
+    let tipo = validarEntrada(prompt("Digite o tipo do vinho:"), "Digite o tipo do vinho:");
+    let quantidade = Number(validarEntrada(prompt("Digite a quantidade:"), "Digite a quantidade:"));
+
+    if (verificarEstoque(quantidade)) {
+        estoqueBaixo++;
+    }   
+
+    mostrarDados(nomeVinho, tipo, quantidade);
+
+    totalCadastros++;
+    let resposta = prompt("Deseja cadastrar outro vinho? (s/n)");
+    if (resposta === null || resposta.toLowerCase() !== "s") {
+        continuar = false;
+    }
+  
+}
